6831b2add531573d1756da734a543fd902e3fede
rummolprod999 <rummolprod999@gmail.com>
2019-12-12 15:35:09 +0300

    add code

---
+++ lib/randomWords.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_app_new/randomWordsState.dart';
+
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => RandomWordsState();
+}
--- lib/randomWordsState.dart
+++ lib/randomWordsState.dart
@@ -1,7 +1,88 @@
-сlass RandomWordsState extends State<RandomWords> {
-@override
-Widget build(BuildContext context) {
-final wordPair = new WordPair.random();
-return new Text(wordPair.asPascalCase);
-}
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_app_new/randomWords.dart';
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _saved =  Set<WordPair>();
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+       MaterialPageRoute(
+        builder: (context) {
+          final tiles = _saved.map(
+            (pair) {
+              return  ListTile(
+                title:  Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+          return  Scaffold(
+            appBar:  AppBar(
+              title:  Text('Saved Suggestions'),
+            ),
+            body:  ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return  ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return  Divider();
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+    return  ListTile(
+      title:  Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing:  Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar:  AppBar(
+        title:  Text('Startup Name Generator'),
+        actions: <Widget>[
+           IconButton(icon:  Icon(Icons.list), onPressed: _pushSaved),
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
+  }
 }
